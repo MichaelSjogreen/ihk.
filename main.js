2903167248c55d32d9d79f32a6d7cf091fa4903f
MichaelSjogreen <147011133+MichaelSjogreen@users.noreply.github.com>
2023-12-20 20:49:17 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+    nombre_alumno-array = [];
+
+
+    function submi()
+    {
+
+
+
+        var nombre_1 = document.getElementById("nombre-alumno-1").value;
+        var nombre_2 = document.getElementById("nombre-alumno-2").value;
+        var nombre_3 = document.getElementById("nombre-alumno-3").value;  
+        var nombre_4 = document.getElementById("nombre-alumno-4").value;
